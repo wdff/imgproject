b0c7d8e55f59b3ea9414d69d08ee3e0702597804
wdff <mpp86@web.de>
2015-06-04 14:54:44 +0200

    newthread und delthread funktionieren

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('documents');
     collection.find({},{}, function(e,docs) {
-        res.render('index', { title: 'Index', "posts": docs });
+        res.render('index', { title: 'Index', posts: docs });
     })
 
 });
@@ -17,7 +17,34 @@ router.get('/newthread', function(req, res) {
 })
 
 router.get('/thread/:id', function(req, res) {
-    res.render('showthread', {title: 'Thread ' + req.params.id})
+    var db = req.db;
+    var collection = db.get('documents');
+    collection.find({_id: req.params.id },function (e,docs) {
+        res.render('showthread', {title: 'Thread ' + req.params.id, posts: docs});
+    })
+})
+
+router.get('/delete', function(req, res) {
+    res.render('deletethread', {title: 'Delete a Thread'});
+})
+
+router.post('/deletethread', function(req, res) {
+    var db = req.db;
+    var threadId = req.body.threadId;
+    var collection = db.get('documents');
+
+    //delete
+    collection.remove({
+        "_id" : threadId
+    }, function (err, doc) {
+        if (err) {
+            res.send("Error while deleting")
+        } else {
+            res.location('/');
+            res.redirect('/');
+        }
+
+    })
 })
 
 /* POST a new thread */
